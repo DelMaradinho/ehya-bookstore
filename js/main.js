331f1dfb745144d9b4e8a2cb7bfeb9b717dfb4d0
DelMaradinho <admitriev91@gmail.com>
2021-07-07 02:00:37 +0300

    create: responsive recomendations and slider

--- js/main.js
+++ js/main.js
@@ -3,6 +3,30 @@ var categoriesSlider = new Swiper('.categories-slider', {
     slidesPerView: 4,
     spaceBetween: 26,
     loop: true,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      },
+      // when window width is >= 767px
+      767:{
+        slidesPerView: 3,
+        spaceBetween: 17
+      },
+      // when window width is >= 576px
+      576:{
+        slidesPerView: 2,
+        spaceBetween: 16
+      },
+      // when window width is >= 320px
+      320:{
+        slidesPerView: 4,
+        slidesPerColumn: 3,
+        spaceBetween: 10
+      }
+    },
   
     // Navigation arrows
     navigation: {
